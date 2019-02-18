ccecf5a3b4abe655cd02fe6a0b914d3b2df27d94
Ryo Yonamine <yonamine@epx.phys.tohoku.ac.jp>
2019-02-18 18:15:55 +0900

    Fix typo.

--- run_DBD/plots/tsys_H20_vector/plot_sensitivity.C
+++ run_DBD/plots/tsys_H20_vector/plot_sensitivity.C
@@ -9,8 +9,8 @@ void plot_sensitivity()
     fname_op1 << "outputs/Sensitivity_Lumi_4000_Lumi_LL_400_Lumi_LR_1600_Lumi_RL_1600_Lumi_RR_400_Mass" << mass << "_2Sigma.root" << ends; 
 
     TFile fin_op1(fname_op1.str().data());
-    //TH1D* h_op1 = static_cast<TH1D*>(fin_op1.Get("Sensitivity Vector"));
-    TH1D* h_op1 = static_cast<TH1D*>(fin_op1.Get("Sensitivity Axial-vector"));
+    TH1D* h_op1 = static_cast<TH1D*>(fin_op1.Get("Sensitivity Vector"));
+    //TH1D* h_op1 = static_cast<TH1D*>(fin_op1.Get("Sensitivity Axial-vector"));
     _mass_op1[index] = double(mass); 
     _lambda_op1[index] = h_op1->GetBinContent(mass); 
     index++;
